eacbcda6f7c9f098b2c9a768dd18415a7a80a72c
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-05 22:41:39 +0530

    Update remove_word.py

--- handlers/remove_word.py
+++ handlers/remove_word.py
@@ -2,8 +2,8 @@ from configs import Config
 async def rmw(cap):
     for i in Config.REMOVE_WORD:
         try:
-            cap1 = cap,replace(f"{i}","")
+            cap1 = cap.replace(f"{i}","")
             cap = cap1
         except:
             pass
-    return cap1
+    return cap
