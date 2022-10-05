cda2231a0bd60e6caa57f465f18d2fb5f15574e1
Ankurhash <91074960+Ankurhash@users.noreply.github.com>
2022-10-05 22:22:24 +0530

    Create remove_word.py

---
+++ handlers/remove_word.py
@@ -0,0 +1,9 @@
+from configs import Config.REMOVE_WORD
+async def rmw(cap):
+    for i in REMOVE_WORD:
+        try:
+            cap1 = cap,replace(f"{i},"")
+            cap = cap1
+        except:
+            pass
+    return cap
